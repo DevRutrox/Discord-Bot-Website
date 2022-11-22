8fe72e0b0010a6aa00365ff6ed4e168f12e59d6e
DevRutrox <118821569+DevRutrox@users.noreply.github.com>
2022-11-22 13:20:24 +0000

    dashboard

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ import { MdAddModerator } from "react-icons/md";
 import { SiProbot } from "react-icons/si";
 import { TbMessage2Code } from "react-icons/tb";
 const config = {
-  name: "ProBot",
+  name: "PritBot",
   //BotName
   inviteLink:
-    "https://discord.com/oauth2/authorize?client_id=879602399067660338&permissions=8&scope=bot&redirect_uri=https://docs.markos.ga&response_type=code",
+    "https://discord.com/api/oauth2/authorize?client_id=1043501821311864862&permissions=8&scope=bot",
   //BotInvite Link
   Features: [
     {
@@ -32,7 +32,7 @@ const config = {
     {
       name: "Welcomer",
       description:
-        "Welcomer is the person who greet the new person in an unique and great way",
+        "Working on it, coming soon...",
       logo: <CgCrown className="h-10 w-10" />,
     },
     // You can added more features if need add
@@ -40,12 +40,9 @@ const config = {
   //Bot Features
   serverCount: 300, //-  - Your
   memberCount: 700, //--   - Bot
-  executedCommand: 27000, //--  - Stats
-  availableCommand: 400, //---   - Here
+  executedCommand: 0, //--  - Stats
+  availableCommand: 0, //---   - Here
   //Do not change if you don't know about them
-  faceBookLogo: (
-    <BsFacebook className="h-10 w-10 text-[#4267B2] hover:scale-125 easeIn cursor-pointer" />
-  ),
   discordLogo: (
     <FaDiscord className="h-10 w-10 text-[#738adb] hover:scale-125 easeIn cursor-pointer" />
   ),
@@ -54,12 +51,9 @@ const config = {
   ),
   //----------------------------------------------------------------
   // Your social handle links
-  githubLink: "https://github.com/mohitmimani",
   twitterLink: "https://twitter.com",
-  mediumLink: "https://medium.com/",
-  facebookLink: "https://facebook.com",
-  discordLink: "https://discord.com",
-  youtubeLink: "https://www.youtube.com/channel/UCfhRHeex9lU0ZAVJot6Z2ZQ",
+  discordLink: "https://discord.gg/dsjn8BFhBf",
+  youtubeLink: "https://www.youtube.com/channel/UC4YnNU_j1i3KlgMTQ_cbYXA",
 };
 
 export default config;
